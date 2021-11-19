65654cf4136d619a93880394da0485474980d03d
edward <edward>
2021-11-19 13:25:37 +0800

    update

--- YKSwiftNetworking/Classes/YKSwiftBaseNetworking.swift
+++ YKSwiftNetworking/Classes/YKSwiftBaseNetworking.swift
@@ -6,7 +6,23 @@
 //
 
 import UIKit
+import Alamofire
 
 class YKSwiftBaseNetworking: NSObject {
+    
+    public static func request(request:YKSwiftNetworkRequest, progressCallBack:(_ progress:Float )->Void, successCallBack:(_ response:YKSwiftNetworkResponse, _ request:YKSwiftNetworkRequest)->Void, failureCallBack:(_ request:YKSwiftNetworkRequest, _ isCache:Bool, _ responseObject:Any, _ error:Error?)->Void)
+    {
+        
+    }
+    
+    public static func upload(request:YKSwiftNetworkRequest, progressCallBack:(_ progress:Float )->Void, successCallBack:(_ response:YKSwiftNetworkResponse, _ request:YKSwiftNetworkRequest)->Void, failureCallBack:(_ request:YKSwiftNetworkRequest, _ isCache:Bool, _ responseObject:Any, _ error:Error?)->Void)
+    {
+        
+    }
+    
+    public static func download(request:YKSwiftNetworkRequest, progressCallBack:(_ progress:Float )->Void, successCallBack:(_ response:YKSwiftNetworkResponse, _ request:YKSwiftNetworkRequest)->Void, failureCallBack:(_ request:YKSwiftNetworkRequest, _ isCache:Bool, _ responseObject:Any, _ error:Error?)->Void)
+    {
+        
+    }
 
 }
