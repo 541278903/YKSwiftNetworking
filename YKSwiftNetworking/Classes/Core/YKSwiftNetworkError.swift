5705df1d124a84a69f04e46459f0061dcde7a324
541278903 <534272374@qq.com>
2023-07-12 18:45:39 +0800

    update error

--- YKSwiftNetworking/Classes/Core/YKSwiftNetworkError.swift
+++ YKSwiftNetworking/Classes/Core/YKSwiftNetworkError.swift
@@ -10,6 +10,24 @@ import Foundation
 
 public enum YKSwiftNetworkError: Error {
     
+    case normal(errorMsg:String)
+    case inCreate(error:Error)
+}
+
+extension YKSwiftNetworkError {
+    
+    internal var underlyingError: Swift.Error? {
+        switch self {
+        case .normal(errorMsg: let errorMsg):
+            return NSError.init(domain: "com.yk.swift.networking", code: -1, userInfo: [
+                NSLocalizedDescriptionKey:errorMsg,
+                NSLocalizedFailureReasonErrorKey:errorMsg,
+                NSLocalizedRecoverySuggestionErrorKey:errorMsg,
+            ])
+        case .inCreate(error: let error):
+            return error
+        }
+    }
 }
 
 extension Error {
